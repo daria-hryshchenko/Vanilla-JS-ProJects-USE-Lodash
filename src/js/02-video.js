40d575f355809674b0e050ebf36d8d3ee2ca9cc9
Daria Hryshchenko <daria199210@gmail.com>
2022-12-29 22:28:30 +0200

    done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import {
+  throttle
+} from 'lodash';
+import Player from "@vimeo/player";
 
+const vimeoPlayerEl = document.querySelector("#vimeo-player");
+
+const player = new Vimeo.Player(vimeoPlayerEl).catch(err => {
+  console.warn(err);
+});
+
+player.on('timeupdate', throttle(event => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000)).catch(err => {
+  console.warn(err);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(err => {
+  console.warn(err);
+});
