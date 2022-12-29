0ea588a67d93a4e4272d5f6decdeecdbd3370002
Daria Hryshchenko <daria199210@gmail.com>
2022-12-29 21:24:23 +0200

    done 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import {
+  galleryItems
+} from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkupEl = createGallery(galleryItems);
+
+galleryEl.innerHTML = galleryMarkupEl;
+
+
+//create elements for gallery
+
+function createGallery(images) {
+  return images.map(({
+      preview,
+      original,
+      description
+    }) =>
+    `<a href=${original} class="gallery__link">
+        <img src=${preview} data-source="large-image.jpg" class="gallery__image" alt=${description}/> 
+        </a>`
+  ).join("");
+};
+
+// check to click a  image  
+galleryEl.addEventListener('click', modalImg);
+
+function modalImg(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionSelector: ".gallery__image",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+gallery.on('error.simplelightbox', function (e) {
+  console.log(e);
+});
