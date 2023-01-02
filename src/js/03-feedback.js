562d79d74858fc880e6b9aca52e0ec789814578e
Daria Hryshchenko <daria199210@gmail.com>
2023-01-03 00:28:14 +0200

    change code task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -50,11 +50,6 @@ const load = key => {
   }
 };
 
-const storageData = load(LOCALSTORAGE_KEY);
-if (storageData) {
-  email.value = storageData.email;
-  message.value = storageData.message;
-}
 
 const remove = key => {
   try {
