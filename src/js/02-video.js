7b7f4e6c96bd709be36586578ec797dec8ae49be
Daria Hryshchenko <daria199210@gmail.com>
2023-01-04 17:26:06 +0200

    change code task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,17 @@
-import throttle from "lodash.throttle";
-import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
-const vimeoPlayerEl = document.querySelector("#vimeo-player");
-const player = new Player(vimeoPlayerEl);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
 
-player.on('timeupdate', throttle(event => {
-  localStorage.setItem(STORAGE_KEY, event.seconds);
-}, 1000));
+player.on('timeupdate', throttle(onVideoPlayer, 1000));
+
+function onVideoPlayer(event) {
+  let dataSrtring = event.seconds;
+  localStorage.setItem(STORAGE_KEY, dataSrtring);
+}
+
 
 player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
