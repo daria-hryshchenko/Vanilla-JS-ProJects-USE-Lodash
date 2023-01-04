2b5251bb22faa8fa07e04c4b921dae694aa69b83
Daria Hryshchenko <daria199210@gmail.com>
2023-01-04 14:09:33 +0200

    change setCurrentTime code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
 import throttle from "lodash.throttle";
 import Player from "@vimeo/player";
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const vimeoPlayerEl = document.querySelector("#vimeo-player");
 const player = new Player(vimeoPlayerEl);
 
 player.on('timeupdate', throttle(event => {
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(STORAGE_KEY, event.seconds);
 }, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
